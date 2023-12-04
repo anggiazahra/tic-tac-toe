1d72cac70e8e7010ab1d639b0af1abcc12a1f3a9
Anggi Nurul Fitriyani Azzahra <anggi.azahra87@gmail.com>
2023-12-04 14:31:09 +0700

    Menambahkan css
    Menambahkan css untuk tampilan game

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
 
+// eslint-disable-next-line react/prop-types
 function Square({value, onSquareClick}) {
-
-  return <button className="square" onClick={onSquareClick}>
+  if (value == 'X') {
+  return <button className="square square-x" onClick={onSquareClick}>
     { value }
+  </button>
+  } else {
+    return <button className="square square-o" onClick={onSquareClick}>
+      { value }
+    </button>
+    }
+}
+
+// eslint-disable-next-line react/prop-types
+function ButtonReset({onResetClick}) {
+  return <button className="btn-reset" onClick={onResetClick}>
+    Reset
   </button>
 }
 
@@ -35,21 +48,29 @@ export default function Board() {
   } else {
     status = 'Next Player : ' + (xIsNext ? 'X' : 'O');
   }
+
+  function reset() {
+    const resetSquares = Array(9).fill(null);
+    setSquares(resetSquares);
+  }
   
   
   return (
     <>
-      <div className='status'>{status}</div>
-      <div className='board'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      <div className='card'>
+        <p className='status'>{status}</p>
+        <div className='board'>
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+        <ButtonReset onResetClick={reset}/>
       </div>
     </>
   )
